fd6ae4da0e99c65fb0238c5530825d051732d1cc
Abilash Subramanian <M1049316@mindtree.com>
2019-05-19 12:14:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { userNameValidator } from '../app/shared/username.validator';
+import { passwordValidator } from './shared/password.validator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AllAboutReactiveForms';
+export class AppComponent implements OnInit {
+
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3), userNameValidator(/password/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: [''],
+      })
+    }, { validator: passwordValidator });
+
+    this.registrationForm.get('subscribe').valueChanges
+      .subscribe(checkedValue => {
+        const email = this.registrationForm.get('email');
+        if (checkedValue) {
+          email.setValidators(Validators.required)
+        }
+        else
+          email.clearValidators();
+        email.updateValueAndValidity();
+      });
+  }
+
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+
+  get email(){
+    return this.registrationForm.get('email');
+
+  }
+
+  loadApi() {
+    this.registrationForm.patchValue({
+      userName: 'Deeksha',
+      address: {
+        city: 'Mangalore',
+        state: 'Karnataka',
+        postalCode: '574214'
+      }
+    });
+  }
 }
